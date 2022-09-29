ce13540dc9bbdf14675028cdedfd66ed14b3a35c
elgarcito <edgarjaguirre@hotmail.com>
2022-09-29 16:31:02 -0300

    bug fix 3

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports= {
     resolve:{
         extensions:['.js','.jsx'],
         alias:{
-            '@components':path.resolve(__dirname,'src/components/.'),
-            '@styles':path.resolve(__dirname,'src/styles/')
+            '@components':path.resolve(__dirname,'src/components/'),
+            '@styles':path.resolve(__dirname,'src/styles')
         }
     },
     module:{
